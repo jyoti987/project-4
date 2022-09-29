62f00268536c4af512046a7bb2e529b4e87d3002
Gouravpundir <pundirgourav0@gmail.com>
2022-09-29 17:17:21 +0530

    created url

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,35 +1,57 @@
-const shortid = require("shortid")
-const validUrl = require("valid-url")
-const urlModel = require("../models/urlModel")
+const shortid = require("shortid");
+const validUrl = require("valid-url");
+const urlModel = require("../models/urlModel");
 
 const createShorturl = async function (req, res) {
+  let data = req.body;
+  let { longUrl } = data;
+  if (Object.keys(longUrl).length == 0) {
+    res
+      .status(400)
+      .send({
+        status: false,
+        message: "Please provide url in the request body",
+      });
+  }
+  const baseUrl = "http://localhost:3000";
+  if (!validUrl.isUri(baseUrl)) {
+    res
+      .status(401)
+      .send({
+        status: false,
+        message: "Please provide url in the request body",
+      });
+  }
 
-    let url = req.body
-
-    if (Object.keys(url).length == 0) {
-        res.status(400).send({ status: false, message: "Please provide url in the request body" })
+  const urlCode = shortid.generate();
+  if (validUrl.isUri(longUrl)) {
+    try {
+      const url = await urlModel.findOne({ longUrl });
+      if (url) {
+        res
+          .status(404)
+          .send({
+            status: false,
+            message: "this url is already exist plz provide different url",
+          });
+      } else {
+        const shortUrl = baseUrl + "/" + urlCode;
+        let obj = {
+          longUrl,
+          shortUrl,
+          urlCode,
+        };
+        const createData = await urlModel.create(obj);
+        res
+          .status(201)
+          .send({ status: true, msg: "created", data: createData });
+      }
+    } catch (err) {
+      res.status(500).send({ status: false, msg: err.msg });
     }
+  } else {
+    res.status(401).send({ status: false, msg: "invalid long" });
+  }
+};
 
-    let { urlCode, longUrl, shortUrl } = url
-
-    if(!urlCode){
-        res.status(400).send({ status: false, message: "Please provide urlCode" })
-    } 
-
-    if(!longUrl){
-        res.status(400).send({ status: false, message: "Please provide longUrl" })
-    }
-
-    const checkUniqueUrl=await urlModel.findOne({longUrl})
-    if(checkUniqueUrl){
-        res.status(404).send({ status: false, message: "this url is already exist plz provide different url" })
-    }
-
-    let baseUrl= "https://localhost:3000"
-
-    let createShortU=baseUrl+"/"+shortid.generate()
-
-
-}
-
-module.exports={createShorturl}
+module.exports = { createShorturl };
